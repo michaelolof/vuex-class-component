3076c09677c6546a7570aebf59fab7588c6370be
michaelolof <myqrrlolof@yahoo.com>
2018-12-23 16:14:34 +0100

    Added proxy support for raw actions @ Sun, Dec 23, 2018  4:14:33 PM

--- dist/getters.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getter(target: any, propertyKey: string): void;
--- dist/mutations.d.ts
+++
@@ -1,2 +0,0 @@
-export declare type MutationDescriptor = TypedPropertyDescriptor<(payload?: any) => void>;
-export declare function mutation<T, U>(target: T, key: string, descriptor: MutationDescriptor): void;
