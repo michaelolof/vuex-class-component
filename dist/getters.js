2f418334e6b853c96b64b6cc100d60b5df7868fa
michaelolof <myqrrlolof@yahoo.com>
2019-01-31 18:58:03 +0100

    Compiled source @ Thu, Jan 31, 2019  6:58:03 PM

--- dist/getters.js
+++ dist/getters.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var _1 = require(".");
 var getterBuilder = function (field) { return new Function("state", "return state." + field); };
 function getter(target, propertyKey) {
-    var ctr = target.constructor();
+    var ctr = Object.getPrototypeOf(new target.constructor());
     if (ctr[_1._getters] === undefined) {
         ctr[_1._getters] = (_a = {},
             _a[propertyKey] = getterBuilder(propertyKey),
